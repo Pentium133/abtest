dc3b606e47cb60eabde6233bafb22d12f79c854d
Serg Puhoff <serg@puhoff.com>
2024-11-20 13:15:06 +0400

    do test work

--- spec/requests/abtest_spec.rb
+++ spec/requests/abtest_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'AP test API', type: :request do
+RSpec.describe 'AB test API', type: :request do
   before(:all) do
     @headers = { 'Device-Token' => '5616245234' }
   end
+
+  let!(:experement) { create(:experement) }
+
   it 'returns a list of AB test' do
-    let (:experement) { create :experement }
     get '/experements', headers: @headers
 
-    puts response.body
+    expect(response).to have_http_status(:ok)
+    puts JSON.parse(response.body)
   end
 end
