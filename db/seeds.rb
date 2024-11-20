2bfc6b1854ff62dd9de298a0d6fc0a6af3419c36
Serg Puhoff <serg@puhoff.com>
2024-11-21 00:52:48 +0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,13 @@ exp1.experement_options << ExperementOption.new(option: '#00FF00', value: '33')
 exp1.experement_options << ExperementOption.new(option: '#0000FF', value: '33')
 
 exp1.save
+
+exp2 = Experement.create(name: 'price')
+
+exp2.experement_options << ExperementOption.new(option: '10', value: '75')
+exp2.experement_options << ExperementOption.new(option: '20', value: '10')
+exp2.experement_options << ExperementOption.new(option: '50', value: '5')
+exp2.experement_options << ExperementOption.new(option: '5', value: '10')
+
+
+exp2.save
